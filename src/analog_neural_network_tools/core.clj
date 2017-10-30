1e20ef41107ecf44c4633a47784602351159beec
Kieran Browne <kierantbrowne@gmail.com>
2017-10-30 15:54:37 +1100

    setup development and print version

--- src/analog_neural_network_tools/core.clj
+++ src/analog_neural_network_tools/core.clj
@@ -3,19 +3,37 @@
             [quil.middleware :as m]))
 
 (defn setup []
+  (q/text-mode :shape)
   {:color 0
    :angle 0})
 
+(defn update [method]
+  (fn [state] {:method method}))
 
-(defn draw []
+(defn draw [state]
   (q/background 255)
   (q/line 0 0 (q/width) (q/height))
-  (q/exit))
-  
+  (when (= :print (:method state)) (q/exit)))
+    
 
-(q/defsketch analog-neural-network-tools
+
+  ;; (q/exit))
+  
+(q/defsketch analog-neural-network-tools-dev
+  :size [500 500]
+  :renderer :java2d
+  :setup setup
+  :update (update :dev)
+  :middleware [m/fun-mode]
+  :draw draw)
+
+
+(q/defsketch analog-neural-network-tools-print
   :size [500 500]
   :renderer :pdf
-  :output-file "test.pdf"
+  :output-file "output.pdf"
   :setup setup
+  :update (update :print)
+  :middleware [m/fun-mode]
   :draw draw)
+)
