a27a9490673e4b7ac5900b23f95389364c55de23
Kieran Browne <kierantbrowne@gmail.com>
2018-02-20 15:17:53 +1100

    moved to fn taking a map

--- src/analog_neural_network_tools/core.clj
+++ src/analog_neural_network_tools/core.clj
@@ -2,65 +2,63 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(def cut [255 0 0])
-(def engrave [0 055 0])
+; (def cut [255 0 0])
+; (def engrave [0 055 0])
+(def cut [255 255 255 0])
+(def engrave [0 0 0])
 
 (defn function-block [fn start end step]
   (q/with-stroke engrave
     (q/begin-shape)
     (doseq [i (range start (+ end step) step)]
-      (q/vertex 
-        (q/map-range i start end 0 (q/width)) 
+      (q/vertex
+        (q/map-range i start end 0 (q/width))
         (q/map-range
           (fn i) -1 1 0 (q/height))))
     (q/end-shape)))
 
 ;; (Math/PI)
 
-(defn function-round [fn start end points max min radius]
+(defn function-round
+  [{:keys [fn start end points max min radius mode] :or {mode :outer}}]
   (q/with-stroke cut
     (q/ellipse 0 0 radius radius))
-    
+
   (q/with-stroke engrave
     (doseq [i points]
-      (q/with-rotation 
-        [(q/map-range 
-           (fn i) 
-           min max 0 (* 2 (Math/PI)))]
+      (q/with-rotation
+        [(q/map-range
+          (fn i)
+          min max 0 (* 2 (Math/PI)))]
         (q/with-fill engrave
-          (let [number (apply str (take 3 (str i)))
-                x (- (* (count number) 2)) 
-                y (- (/ radius 2) 20)]
+          (let [number (apply str (take (if (< i 0) 4 3) (str i)))
+                x (- (* (count number) (if (rational? i) 5 3)))
+                y ((case mode :inner + :outer -) (/ radius 2) 20)]
 
-            (q/text-size 8)
-            (when true 
+            (if (rational? i)
+              (q/text-size 12)
+              (q/text-size 8))
+            (when true
               (q/text number (+ x)  y))))
-            ; (when 
-            ;   (mod i (quot (fn i)))
-
-
-            ;   (q/text "text " 20 20))))
 
         (q/no-fill)
-
         (when (rational? i)
           (q/stroke-weight 2))
-        (q/line 0 (/ radius 2) 0 (- (/ radius 2) (if (rational? i) 10 4)))
+        (q/line 0 (/ radius 2)
+                0 (- (/ radius 2)
+                     ((case mode :inner - :outer +)
+                      (if (rational? i) 10 4))))
         (q/stroke-weight 1)))))
-        
-(count "1 2 3")
 
-(count)
-      
-      
+
+
 
 (defn sigmoid [x]
   (Math/tanh x))
-  
+
 (defn log [x]
   (Math/log x))
-  
-(Math/log 10)
+
 
 (defn setup []
   (q/text-mode :shape)
@@ -84,24 +82,45 @@
     (q/with-stroke [0]
       (q/point 0 0))
     ;; (function-round sigmoid -4 4 0.2 -1 1 300)
-    (function-round log 1 10 (range 1 10) 0 2.302 380)
-    (function-round log 1 10 (range 1 2 0.1) 0 2.302 380)
-    (function-round log 1 10 (range 2 3 0.2) 0 2.302 380))
+    ; (function-round log 1 10 (range 1 10) 0 2.302 600)
+    ; (function-round log 1 10 (range 1 2 0.1) 0 2.302 600)
+    ; (function-round log 1 10 (range 2 3 0.2) 0 2.302 600)
+    ; (function-round log 1 10 (range 1 10) 0 2.302 800)
+    (q/ellipse 0 0 700 700)
+    (function-round
+     {:fn log :start 1 :end 10
+      :points (range 1 10)
+      :max 0 :min 2.302 :radius 700})
+    (function-round
+     {:fn log :start 1 :end 10
+      :points (range 1 2 0.1)
+      :max 0 :min 2.302 :radius 700})
+    (function-round
+     {:fn log :start 1 :end 10
+      :points (range 2 3 0.2)
+      :max 0 :min 2.302 :radius 700})
+    ;; multiplier
+    (q/ellipse 0 0 800 800)
+    (function-round
+     {:fn identity :start 1 :end 10 :points (range -1 1 0.1) :max -1 :min 1 :radius 800 :mode :inner})
+    (function-round
+     {:fn identity :start 1 :end 10 :points (range -1 1 0.1) :max -1 :min 1 :radius 800})
+    )
   (when (= :print (:method state)) (q/exit)))
-  
+
 (q/defsketch analog-neural-network-tools-dev
-  :size [700 700]
+  :size [880 880]
   :renderer :java2d
   :setup setup
   :update (update :dev)
   :middleware [m/fun-mode]
   :draw draw)
 
-(q/defsketch analog-neural-network-tools-print
-  :size [700 700]
-  :renderer :pdf
-  :output-file "output.pdf"
-  :setup setup
-  :update (update :print)
-  :middleware [m/fun-mode]
-  :draw draw)
+;; (q/defsketch analog-neural-network-tools-print
+;;   :size [700 700]
+;;   :renderer :pdf
+;;   :output-file "output.pdf"
+;;   :setup setup
+;;   :update (update :print)
+;;   :middleware [m/fun-mode]
+;;   :draw draw)
