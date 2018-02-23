1b1325e38008901364444c43f21810eccc5bcabb
Kieran Browne <kierantbrowne@gmail.com>
2018-02-23 18:52:21 +1100

    prepared for lazer cutting

--- src/analog_neural_network_tools/core.clj
+++ src/analog_neural_network_tools/core.clj
@@ -3,12 +3,11 @@
             [quil.middleware :as m]))
 
 (def cut [255 0 0])
-; (def engrave [0 055 0])
-;; (def cut [255 255 255 0])
-(def engrave [0 0 0])
+(def etch [0 0 255])
+(def raster-etch [0 0 0])
 
 (defn function-block [fn start end step]
-  (q/with-stroke engrave
+  (q/with-stroke etch
     (q/begin-shape)
     (doseq [i (range start (+ end step) step)]
       (q/vertex
@@ -23,7 +22,7 @@
   [{:keys [from-d to-d rotation]}]
   (q/with-rotation [rotation]
     (q/with-stroke cut
-      (q/stroke-weight 1)
+      ;; (q/stroke-weight 1)
       (q/begin-shape)
       (q/vertex 0 (* -1/2 from-d))
 
@@ -47,26 +46,34 @@
   )
 
 (defn function-round
-  [{:keys [fn start end points max min radius mode numbers? tick-size text-fn] :or {mode :outer numbers? true tick-size 4 text-fn identity}}]
+  [{:keys [fn start end points max min radius mode numbers? tick-size text-fn] :or {mode :outer numbers? true tick-size (* 0.005 (q/height)) text-fn identity}}]
   ;; (q/with-stroke cut
   ;;   (q/ellipse 0 0 radius radius))
 
-  (q/with-stroke engrave
+  (q/with-stroke etch
     (doseq [i points]
       (q/with-rotation
         [(q/map-range
           (fn i)
           min max 0 (* 2 (Math/PI)))]
-        (q/with-fill engrave
-          (let [number (apply str (take (if (< (text-fn i) 0) 4 3)
-                                        (str (text-fn i))))
-                x (- (* (count number) (if (rational? i) 3 2)))
+        (q/with-fill etch
+          (let [number (apply str
+                              (take (if (< (text-fn i) 0) 4 3)
+                                    (str (text-fn i))))
+                x (- (* (count number)
+                        (if (rational? i)
+                          (* 0.0015 (q/height))
+                          (* 0.002 (q/height))
+                            )))
                 y ((case mode :inner + :outer -) (* 1/2 radius)
-                   (if (rational? i) 18 12))]
+                   (if (rational? i)
+                     (* 0.028 (q/height))
+                     (* 0.02 (q/height))
+                     ))]
 
             (if (rational? i)
-              (q/text-size 12)
-              (q/text-size 8))
+              (q/text-size (* 0.01 (q/height)))
+              (q/text-size (* 0.005 (q/height))))
             (q/push-matrix)
             (q/translate (+ x) y)
             (when (= mode :inner)
@@ -74,19 +81,18 @@
               (q/rotate Math/PI)
               )
             (when numbers?
-              (q/text number 0 0))
+              (q/with-fill raster-etch
+                (q/text number 0 0)))
             (q/text-align :left)
             (q/pop-matrix)
             ))
 
         (q/no-fill)
-        (when (rational? i)
-          (q/stroke-weight 2))
         (q/line 0 (/ radius 2)
                 0 (- (/ radius 2)
                      ((case mode :inner - :outer +)
-                      (if (rational? i) 10 tick-size))))
-        (q/stroke-weight 1)))))
+                      (if (rational? i) (* 0.014 (q/height)) tick-size))))
+        ))))
 
 
 
@@ -102,10 +108,11 @@
   (q/text-mode :shape)
   (q/no-fill)
   {:color 0
-   :angle 0})
+   :angle 0
+   :method :dev})
 
 (defn update [method]
-  (fn [state] {:method method}))
+  (constantly {:method method}))
 
 ;; (def mult-outer
 ;;    {:fn log :start 1 :end 10
@@ -122,7 +129,7 @@
 ;;     :max 0 :min 2.302 :radius 700}
 ;;   )
 
-(def mult-outer
+(defn mult-outer []
   ((juxt
     identity
     #(assoc % :points (range 1 10 0.1) :numbers? false)
@@ -130,15 +137,22 @@
     #(assoc % :points (range 2 3 0.2)))
    {:fn log :start 1 :end 10
     :points (range 1 10)
-    :max 0 :min 2.302 :radius 700}))
+    :max 0 :min 2.302 :radius (* 0.84 (q/height))}))
 
-(def add-outer
+(defn add-outer []
   ((juxt
     identity
     #(assoc % :points (range -9 9 0.1) :numbers? false)
     #(assoc % :points (range -9 9 0.05) :numbers? false :tick-size 2))
    {:fn identity :points (range -9 10 1) :max 10 :min -10 :radius 500}))
 
+(defn indicator [r mode]
+  ;; (q/ellipse 0 0 (* 0.46 (q/height)) (* 0.46 (q/height)))
+  (q/triangle
+   0 (* -1/2 r (q/height))
+   (*  0.004 (q/height)) ((case mode :inward - +) (* -1/2 r (q/height)) (* 0.01 (q/height)))
+   (* -0.004 (q/height)) ((case mode :inward - +) (* -1/2 r (q/height)) (* 0.01 (q/height)))
+  ))
 
 (defn draw [state]
   (q/background 255)
@@ -149,20 +163,37 @@
   ;   (q/text "i" 34 20))
   ;; (function-block sigmoid -4 4 0.2)
   (q/no-fill)
-  (q/with-translation [(/ (q/width) 2) (/ (q/height) 2)]
+  (q/with-translation [(* (q/width) 0.25) (/ (q/height) 2)]
     (when (= (:method state) :print) (q/scale 0.7))
+    ;; (when (= (:method state) :dev) (q/scale 0.3))
 
 
     ; draw outer multiplier disc
-    (q/ellipse 0 0 800 800)
+    (q/stroke-weight 0.1)
+    (q/with-stroke cut
+      ;; edge of device
+      (q/ellipse 0 0 (* 0.96 (q/height)) (* 0.96 (q/height)))
+      (q/with-translation [(* 1/2 (q/width)) 0]
+        (q/ellipse 0 0 (* 0.96 (q/height)) (* 0.96 (q/height)))
+
+        (q/with-stroke etch
+          (q/ellipse 0 0 (* 0.72 (q/height)) (* 0.72 (q/height)))
+          (q/ellipse 0 0 (* 0.7 (q/height)) (* 0.7 (q/height)))
+          (q/ellipse 0 0 (* 0.46 (q/height)) (* 0.46 (q/height)))
+          (q/ellipse 0 0 (* 0.44 (q/height)) (* 0.44 (q/height))))
+        )
+      )
+    ;; (q/ellipse 0 0 800 (q/height))
     (mapv function-round
           (map #(assoc % :mode :inner)
-               mult-outer))
+               (mult-outer)))
     ; draw inner multiplier disc
-    (q/ellipse 0 0 700 700)
-    (q/rotate 4)
-    (mapv function-round mult-outer)
-    (q/ellipse 0 0 600 600)
+    (q/with-stroke cut
+      (q/ellipse 0 0 (* 0.84 (q/height)) (* 0.84 (q/height))))
+    (mapv function-round (mult-outer))
+
+    (q/with-stroke cut
+      (q/ellipse 0 0 (* 0.72 (q/height)) (* 0.72 (q/height))))
 
     ;; (puzzle-join
     ;;  {:from-d 700 :to-d 800 :rotation 1})
@@ -171,51 +202,64 @@
 
     ;; summer
     ;; outer marker
-    ;; (q/ellipse 0 0 600 600)
-    (q/with-fill engrave
-      (q/triangle 0 -296 -2 -300 2 -300)
-      ;; (q/triangle 0 -204 -2 -200 2 -200)
-      )
-    (q/ellipse 0 0 591 591)
+    (q/with-stroke cut
+      (q/ellipse 0 0 (* 0.7 (q/height)) (* 0.7 (q/height))))
+    ;; (q/with-fill etch
+    ;;   (q/triangle 0 -296 -2 -300 2 -300)
+    ;;   ;; (q/triangle 0 -204 -2 -200 2 -200)
+    ;;   )
 
-    ; draw inner add disc
-    ;; (q/ellipse 0 0 591 591)
+    (q/with-stroke cut
+      (q/ellipse 0 0 (* 0.58 (q/height)) (* 0.58 (q/height))))
+
+    ; draw outer add disc
     (mapv function-round
-          (map #(assoc % :radius 590 :numbers? false)
-               add-outer))
+          (map #(assoc % :radius (* 0.7 (q/height)) :numbers? false)
+               (add-outer)))
     (mapv function-round
-          (map #(assoc % :mode :inner)
-               add-outer))
-    (q/rotate 4)
-    (q/ellipse 0 0 500 500)
-    ;; ; draw outer add disc
-    ;; (q/ellipse 0 0 500 500)
+          (map #(assoc % :radius (* 0.58 (q/height)) :mode :inner)
+               (add-outer)))
+    ;; (q/rotate 4)
+
+    (q/with-stroke cut
+      (q/ellipse 0 0 (* 0.46 (q/height)) (* 0.46 (q/height)))
+      )
+    ;; ; draw inner add disc
     (mapv function-round
-          (map #(assoc % :numbers? false) add-outer))
+          (map #(assoc % :radius (* 0.58 (q/height)) :numbers? false) (add-outer)))
     (mapv function-round
-          (map #(assoc % :radius 410 :mode :inner :fn - :text-fn (partial max 0))
-               add-outer)
+          (map #(assoc % :radius (* 0.46 (q/height)) :mode :inner :fn - :text-fn (partial max 0))
+               (add-outer))
           )
-    (q/ellipse 0 0 410 410)
+    (q/with-stroke cut
+      ;; (q/ellipse 0 0 (* 0.44 (q/height)) (* 0.44 (q/height)))
+      )
+    ;; (q/blend
+    ;;  (q/load-image "snake.png")
+    ;;  0 0 2100 3431
+    ;;  (* 0.32 (q/width))  (* 0.49 (q/height)) 300 400
+    ;;  :multiply)
+    ;; (q/ellipse 0 0 410 410)
 
     ; inner draw sum indicator
-    (q/ellipse 0 0 410 410)
-    (q/with-fill engrave
-      (q/triangle 0 -204 -2 -200 2 -200))
-    (q/ellipse 0 0 400 400)
+    ;; (q/ellipse 0 0 410 410)
+    (q/with-fill raster-etch
+      (indicator 0.7 :inward)
+      (indicator 0.46 :outward)
+      )
+    ;; (q/ellipse 0 0 400 400)
     ; draw centre point
     ;; (q/with-stroke [0]
     ;;   (q/ellipse 0 0 2 2))
-    ;; (function-round
-    ;;  {:fn identity :start 1 :end 10 :points (range -1 1 0.1) :max -1 :min 1 :radius 600})
+    ;; function-round
     ;; (function-round
     ;;  {:fn identity :start 1 :end 10 :points (range -1 1 0.1) :max -1 :min 1 :radius 800})
     )
   ;; (q/save "test.png")
-  (when (= :print (:method state)) (q/exit)))
+  (when-not (= (:method state) :dev) (q/exit)))
 
 (q/defsketch analog-neural-network-tools-dev
-  :size [880 880]
+  :size [900 450]
   :renderer :java2d
   :setup setup
   :update (update :dev)
@@ -228,5 +272,16 @@
   :output-file "output.pdf"
   :setup setup
   :update (update :print)
+  :middleware [m/fun-mode]
+  :draw draw)
+
+(def mm->300dpi (partial map (comp int #(Math/ceil %) (partial * 11.81))))
+
+(q/defsketch analog-neural-network-tools-lazer-cut
+  :size (mm->300dpi [300 150])
+  :renderer :pdf
+  :output-file "output.pdf"
+  :setup setup
+  :update (update :lazer-cut)
   :middleware [m/fun-mode]
   :draw draw)
