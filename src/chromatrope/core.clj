247635d94b580e462f7eb4c705306e7f5eb3c346
Kieran Browne <kierantbrowne@gmail.com>
2018-03-15 01:07:31 +1100

    added pdf rednerer

--- src/chromatrope/core.clj
+++ src/chromatrope/core.clj
@@ -2,25 +2,40 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(def cut [0 0 0])
-(def etch [200 200 200])
-(def raster-etch [200 200 200])
-
-(defn setup []
-  ;; (q/text-mode :shape)
-  {:color 0
-   :angle 0
-   :method :dev})
-
-(defn update [method]
-  (constantly {:method method}))
-
-
 (defn centre [] [(* (q/width) 0.5) (* (q/height) 0.5)])
+
+(defmacro do-around [n-times & body]
+  `(dotimes [i# ~n-times]
+     (q/with-rotation [(* i# (/ (Math/PI) 0.5 ~n-times))]
+       ~@body)))
+  
+(defn star 
+  ([n-points] (star n-points 0.5))
+  ([n-points tuck]
+   (let [full (* 0.45 (q/height))
+          tuck (* 2 tuck)
+          theta (q/radians (* 1/2 (/ 360 n-points))) 
+          a (* 1/2 full)
+          h (/ a (q/cos theta))
+          o (* h (q/sin theta))]
+
+      (do-around n-points
+          (q/quad
+            0 0 
+            (* tuck o) (* tuck a)
+            0 full
+            (- (* tuck o)) (* tuck a))))))
+
+(defn no-1 []
+  ;; (q/fill 0 255 0)
+  ;; (q/fill 0 255 0)
+  ;; (q/stroke 0 20 25)
+  (star 6))
   
 (defn no-2 []
-  (q/fill 220 70 20)
-  (let [full (* 0.5 (q/height))
+  ;; (q/fill 220 70 20)
+  (let [full (- (* 0.5 (q/height)) 40)
+
         middle (* 0.5 full)
         onethird (* 1/3 full)
         twothirds (* 2/3 full)
@@ -38,41 +53,6 @@
 
         ;; (q/bezier-vertex 0 0 400 400 0 0)
         (q/end-shape)))))
-
-(defmacro do-around [n-times & body]
-  `(dotimes [i# ~n-times]
-     (q/with-rotation [(* i# (/ (Math/PI) 0.5 ~n-times))]
-       ~@body)))
-       
-  
-(defn star 
-  ([n-points] (star n-points 0.5))
-  ([n-points tuck]
-   (let [full (* 0.5 (q/height))
-          tuck (* 2 tuck)
-          theta (q/radians (* 1/2 (/ 360 n-points))) 
-          a (* 1/2 full)
-          h (/ a (q/cos theta))
-          o (* h (q/sin theta))]
-
-      (do-around n-points
-          (q/quad
-            0 0 
-            (* tuck o) (* tuck a)
-            0 full
-            (- (* tuck o)) (* tuck a))))))
-
-        
-
-        
-  
-
-(defn no-1 []
-  ;; (q/fill 0 255 0)
-  (q/fill 0 255 0)
-  (q/stroke 0 20 25)
-  (star 6))
-  
 
 (defn no-6 []
   (q/fill 205 0 205)
@@ -106,28 +86,27 @@
   (q/translate (centre))
   (q/fill 255)
   (do-around 46 (q/ellipse (* 1/2 (q/width)) (* 1/2 (q/width)) 169 169))
-  (q/blend-mode :subtract)
+  (when (= (:method state) :dev) (q/blend-mode :subtract))
   (q/with-rotation [(/ (q/frame-count) -18)]
     (show-slide 0 (:slides state)))
   (q/with-rotation [(/ (q/frame-count) 40)]
 
     (show-slide 1 (:slides state)))
   (q/with-rotation [(/ (q/frame-count) -90)]
-    (show-slide 5 (:slides state))))
-  
-    
-    
-
-    
+    (show-slide 5 (:slides state)))
   ;; (q/save "test.png")
-  ;; (when-not (= (:method state) :dev) (q/exit)))
+  (when-not (= (:method state) :dev) (q/exit)))
 
-(nth (repeat 1) 4)
 
-(q/defsketch analog-neural-network-tools-dev
+(q/defsketch chromatrope-dev
   :size [400 400]
   :renderer :p2d
-  :setup (fn [] {:slides (repeatedly #(future (q/create-graphics 400 400 :p2d)))})
+  :setup (fn [] {:slides (repeatedly #(future (q/create-graphics 400 400 :p2d)))
+                 :method :dev
+                 :cut [0 0 0]
+                 :etch [255 0 0]
+                 :raster-etch [0 0 255]})
+                 
   :update identity
   :draw draw
   :middleware [m/fun-mode])
@@ -142,13 +121,28 @@
 ;   :middleware [m/fun-mode]
 ;   :draw draw)
 
-; (def mm->300dpi (partial map (comp int #(Math/ceil %) (partial * 11.81))))
+(def mm->300dpi (partial map (comp int #(Math/ceil %) (partial * 11.81))))
 
-; (q/defsketch analog-neural-network-tools-lazer-cut
-;   :size (mm->300dpi [300 150])
-;   :renderer :pdf
-;   :output-file "output.pdf"
-;   :setup setup
-;   :update (update :lazer-cut)
-;   :middleware [m/fun-mode]
-;   :draw draw)
+(q/defsketch chromatrope-lazer-cut
+  :size (map (partial + 80) (mm->300dpi [80 80]))
+  :renderer :pdf
+  :output-file "output.pdf"
+  :setup (fn [] 
+           (q/background 255)
+           (q/text-mode :shape) 
+           (q/stroke-weight 0.1)
+           (q/no-fill) {:bleed 80})
+  ;:slides (repeatedly #(future (q/create-graphics 400 400 :pdf)))
+                 ;:method :lazer-cut
+                 ;:cut [0 0 0]
+                 ;:etch [255 0 0]
+                 ;:raster-etch [0 0 255]})
+  :update #(assoc % :method :lazer-cut)
+  :middleware [m/fun-mode]
+  :draw (fn [state] 
+          (q/with-translation (centre) 
+            (q/stroke 255 0 0)
+            (q/ellipse 0 0 (- (q/width) (:bleed state)) (- (q/height) (:bleed state)))
+            (q/stroke 0 0 255)
+            (no-2)) 
+          (if (= (:method state) :lazer-cut) (q/exit))))
