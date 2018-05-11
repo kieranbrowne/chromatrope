7543d7a68c3f6b67ef29b955d6d405f09bc0509d
Kieran Browne <kierantbrowne@gmail.com>
2018-05-11 16:09:42 +1000

    wibbly wobbly

--- src/chromatrope/core.clj
+++ src/chromatrope/core.clj
@@ -46,26 +46,25 @@
   (q/stroke 0)
   (star 6))
 
-(defn no-2 []
+(defn no-2 [num in-r out-r]
   ;; (q/fill (colour-phase 0.01) (colour-phase 0.2) (colour-phase 0.08))
   ;; (q/fill 0.2 1 2)
   ;; (q/stroke (colour-phase 0.01) (colour-phase 0.2) (colour-phase 0.08))
   ;; (q/stroke 0)
-  (let [full (- (* 0.5 (q/height)) 40)
+  (let [full out-r
 
-        middle (* 0.5 full)
-        onethird (* 1/3 full)
-        twothirds (* 2/3 full)
+        middle (q/map-range 0.3 0 1 in-r out-r)
+        onethird (q/map-range 1/3 0 1 in-r out-r)
+        twothirds (q/map-range 2/3 0 1 in-r out-r)
         thick (* 0.9 middle)
-        thin (* 0.8 middle)
-        num-parts 30]
+        thin (* 0.6 middle)]
 
-    (dotimes [i num-parts]
-      (q/with-rotation [(* i (/ (Math/PI) 0.5 num-parts))]
+    (dotimes [i num]
+      (q/with-rotation [(* i (/ (Math/PI) 0.5 num))]
         (q/begin-shape)
-        (q/vertex 0 0)
+        (q/vertex 0 in-r)
         (q/bezier-vertex thick onethird thick twothirds 0 full)
-        (q/bezier-vertex thin twothirds thin onethird 0 0)
+        (q/bezier-vertex thin twothirds thin onethird 0 in-r)
         ;; (q/bezier-vertex 40 (* 0.25 (q/height)) 40 (* 0.5 (q/height)) 0 0)
 
         ;; (q/bezier-vertex 0 0 400 400 0 0)
@@ -124,7 +123,7 @@
 
   (q/fill 1)
   (q/ellipse 0 0 380 380)
-  ;; (q/display-filter :dilate)
+  ;; (q/display-filter :blur)
   (q/blend-mode :darkest)
 
   (q/no-stroke)
@@ -140,83 +139,216 @@
   (q/fill 1 1 1)
   (q/stroke 0)
   (q/no-stroke)
-  (q/with-rotation [(/ (q/frame-count) 255)]
-    (do-around 20
-               (q/fill 0 1 1)
-               (q/quad 20 20 50 80 32 82 20 28)
-               (q/fill 0.5 1 1)
-               (q/quad 00 00 20 20 32 22 00 0)
-               (q/fill 0.9 1 0.9)
-               (q/quad 80 80 120 80 92 92 90 90)
-               (q/quad 70 70 110 70 82 82 80 80)
+  (q/with-rotation [(/ (q/frame-count) 100)]
+    (do-around 4
+               (q/fill 0.0 1 1)
+               (q/stroke-weight 4)
+               (q/stroke 0)
+               (q/stroke-join :bevel)
+               ;; (q/stroke 0.0 1 1)
+               (q/line 0 50  30 20)
+               (q/line 0 60  40 20)
+               ;; (q/stroke 0.65 1 1)
+               (q/line 0 50 -20 30)
+               (q/line 0 60 -30 30)
+               ;; (q/stroke-weight 10)
+               ;; (q/stroke 0.0 1 1)
+               ;; (q/line 0 90 -100 80)
+               ;; (q/line 0 120 -120 90)
+
+               (q/no-fill)
+               ;; (q/rect 0 2 20 20)
+               ;; (q/stroke 0.65 1 1)
+               (let [s 20]
+                 (q/line 0 s s s)
+                 (q/line s s s 0)
+                 )
+               ;; (q/stroke 0.65 1 1)
+               (q/stroke-weight 2)
+               ;; (q/stroke 0 1 1)
+               (let [s 16]
+                 (q/line 0 s s s)
+                 (q/line s s s 0)
+                 )
+               ;; (q/stroke 0.65 1 1)
+               (let [s 13]
+                 (q/line 0 s s s)
+                 (q/line s s s 0)
+                 )
+               ;; (q/stroke 0 1 1)
+               (let [s 10]
+                 (q/line 0 s s s)
+                 (q/line s s s 0)
+                 )
+               ;; (q/stroke 0.65 1 1)
+               (let [s 7]
+                 (q/line 0 s s s)
+                 (q/line s s s 0)
+                 )
+               ;; (q/stroke 0 1 1)
+               (let [s 4]
+                 (q/line 0 s s s)
+                 (q/line s s s 0)
+                 )
+
+
+               (q/stroke 0)
+               (q/stroke-weight 6)
+               (q/stroke-cap :square)
+               ;; (q/stroke 0.0 1 1)
+               (doall
+                (for [i (range 10)]
+                  (q/line 0 60
+                          (* 120 (q/sin (/ i 6)))
+                          (* 120 (q/cos (/ i 6))))))
+               ;; (q/no-fill)
+               ;; (q/ellipse  0 0 390 280)
+               ;; (q/fill 0)
+               ;; (q/quad 20 00 30 190 82 180 30 28)
+               ;; (q/quad 20 00 70 190 22 179 30 28)
+               ;; (q/fill 0.5 1 1)
+               ;; (q/quad 00 00 20 20 32 22 00 0)
+               ;; (q/fill 0.0 1 0.9)
+               ;; (q/quad 80 80 120 80 92 92 90 90)
+               ;; (q/quad 70 70 110 70 82 82 80 80)
+
                )
-    ;; (no-2)
+    ;; (do-around 8
+    ;;            (q/stroke 0.25 1 1)
+    ;;            (q/rotate 0.8)
+    ;;            ;; (q/stroke-weight 8)
+    ;;            (q/no-fill)
+    ;;            (q/ellipse  0 0 390 280)
+    ;;            )
+    ;; (q/stroke 0.65 1 1)
+    (q/stroke-weight 9)
+    (do-around 40
+               (q/bezier 0 120
+                         90 140
+                         -10 170
+                         48 174))
+    (q/fill 0)
+    ;; (no-2 30 130 180)
+    ;; (no-2 10 80 120)
+    ;; (no-2 20 50 90)
+    ;; (no-2 26 00 60)
+    (q/no-stroke)
+    ;; (q/fill 0.0 1 1)
+    ;; (no-2 8 80 180)
+    (do-around 40 (q/quad 0 180 11 200 18 200 6 180))
     )
 
 
 
   ;; slot two
-  (q/with-rotation [(/ (q/frame-count) -255)]
+  (q/with-rotation [(/ (q/frame-count) -100)]
     (q/scale -1 1)
-    (do-around 20
-               (q/fill 0.5 1 1)
-               (q/quad 20 20 50 80 32 82 20 28)
-               (q/fill 0.0 1 1)
-               (q/quad 00 00 20 20 32 22 00 0)
-               (q/fill 0.4 1 0.7 0.1)
-               (q/quad 80 80 120 80 92 92 90 90)
-               (q/quad 70 70 110 70 82 82 80 80)
+    (do-around 4
+               (q/stroke-weight 4)
+               (q/stroke-join :bevel)
+               (q/stroke 0)
+               ;; (q/fill 0)
+
+               ;; (q/stroke 0.65 1 1)
+               (q/line 0 50  30 20)
+               (q/line 0 60  40 20)
+               ;; (q/stroke 0.0 1 1)
+               (q/line 0 50 -20 30)
+               (q/line 0 60 -30 30)
+
+               (let [s 20]
+                 (q/line 0 s s s)
+                 (q/line s s s 0))
+
+               (q/no-fill)
+               ;; (q/rect 2 0 20 20)
+               (q/stroke-weight 7)
+               ;; (q/stroke 0.65 1 1)
+               (doall
+                (for [i (range 10)]
+                  (q/line 0 60
+                          (* 120 (q/sin (/ i 6)))
+                          (* 120 (q/cos (/ i 6))))))
+               (q/stroke-weight 2)
+               ;; (q/stroke 0.65 1 1)
+               (let [s 16]
+                 (q/line 0 s s s)
+                 (q/line s s s 0)
+                 )
+               ;; (q/stroke 0 1 1)
+               (let [s 13]
+                 (q/line 0 s s s)
+                 (q/line s s s 0)
+                 )
+               ;; (q/stroke 0.65 1 1)
+               (let [s 10]
+                 (q/line 0 s s s)
+                 (q/line s s s 0)
+                 )
+               ;; (q/stroke 0 1 1)
+               (let [s 7]
+                 (q/line 0 s s s)
+                 (q/line s s s 0)
+                 )
+               ;; (q/stroke 0.65 1 1)
+               (let [s 4]
+                 (q/line 0 s s s)
+                 (q/line s s s 0)
+                 )
+
+               ;; (q/stroke 0.65 1 1)
+               ;; (q/stroke-weight 10)
+               ;; (q/line 0 90 -100 80)
+               ;; (q/line 0 120 -120 90)
+               ;; (q/no-fill)
+               ;; (q/ellipse  0 0 390 280)
+               ;; (q/quad 20 00 30 190 82 280 20 28)
+               ;; (q/quad 20 00 40 190 22 122 30 28)
+               ;; (q/fill 0.0 1 1)
+               ;; (q/quad 00 00 20 20 32 22 00 0)
+               ;; (q/fill 0.4 1 0.7 0.1)
+               ;; (q/quad 80 80 120 80 92 92 90 90)
+               ;; (q/quad 70 70 110 70 82 82 80 80)
                )
-    ;; (no-2)
+    ;; (do-around 9
+    ;;            (q/no-stroke)
+    ;;            (q/quad 40 40 42 50 90 50 42 40)
+    ;;            )
+    (q/stroke-weight 4)
+    ;; (q/stroke 0.0 1 1)
+    (q/stroke-weight 9)
+    (do-around 40
+               (q/bezier 0 120
+                         90 140
+                         -10 170
+                         48 174))
+    (q/no-stroke)
+    (q/fill 0)
+    ;; (q/fill 0.65 1 1)
+    ;; (q/stroke 0.65 1 1)
+    (q/stroke-weight 3)
+    (do-around 40 (q/quad 0 180 11 200 18 200 6 180))
+    ;; (no-2 22 90 100)
+    ;; (no-2 20 50 90)
     )
 
   (q/blend-mode :blend)
   ;; (q/blend-mode :normal)
   ;; (q/display-filter :invert)
 
-  ;; (draw-slide 0 no-1 (:slides state))
-  ; (draw-slide 1 no-2 (:slides state))
-  ; (draw-slide 5 no-6 (:slides state))
-  ; (draw-slide 6 no-7 (:slides state))
-  ; (when (= 0 (mod (q/frame-count) 50))
-  ;   (q/blend-mode (:shader state)))
-
-  ; (q/fill 0)
-  ; (q/no-stroke)
-  ; (do-around 46 (q/ellipse (* 1/2 (q/width)) (* 1/2 (q/width)) 169 169))
-  ;; (when (= (:method state) :dev) (q/blend-mode :exclusion))
-  ; (q/with-rotation [(/ (q/frame-count) 55)]
-  ;   (show-slide 0 (:slides state)))
-  ; (q/with-rotation [(/ (q/frame-count) 380)]
-  ;   (show-slide 1 (:slides state)))
-  ;; (q/load-pixels)
-  ;; (println 1)
-  ; (let [pix (q/pixels)
-  ;       a (q/pixels (deref (nth (:slides state) 0)))
-  ;       b (q/pixels (deref (nth (:slides state) 1)))]
-  ;   (dotimes [i (* 400 400)]
-  ;     (aset pix i (+ (aget a i) (aget b i)))))
-  ; (q/update-pixels)
-
-  ; (q/with-rotation [(/ (q/frame-count) 155)]
-  ;   (show-slide 1 (:slides state)))
-  ; (q/with-rotation [(/ (q/frame-count) -60)]
-  ;   (show-slide 5 (:slides state)))
-  ; (when (= (:method state) :dev) (q/blend-mode :darkest))
-  ; (q/with-rotation [(/ (q/frame-count) 820)]
-  ;   (show-slide 1 (:slides state)))
-  ; (q/with-rotation [(/ (q/frame-count) -200)]
-  ;   (show-slide 6 (:slides state)))
 
   ;; (q/save "test.png")
-  ;; (q/save-frame  "frame-####.png")
+  (if (and (>= (q/frame-count) 10)
+           (< (q/frame-count) 100)
+           )
+    (q/save-frame  "frame-####.png"))
   (when-not (= (:method state) :dev) (q/exit)))
 
 
 (q/defsketch chromatrope-dev
   :size [400 400]
   :renderer :p2d
-  :settings (fn [] (q/pixel-density 1) (q/smooth))
+  :settings (fn [] (q/pixel-density 2) (q/smooth))
   :setup (fn []
            (q/color-mode :hsb 1.0)
            {:slides (repeatedly #(future (q/create-graphics 400 400 :p2d)))
@@ -235,14 +367,6 @@
   :middleware [m/fun-mode])
 
 
-; (q/defsketch analog-neural-network-tools-print
-;   :size [595 842]
-;   :renderer :pdf
-;   :output-file "output.pdf"
-;   :setup setup
-;   :update (update :print)
-;   :middleware [m/fun-mode]
-;   :draw draw)
 
 (def mm->300dpi (partial * 11.81))
 
@@ -267,5 +391,6 @@
             (q/stroke 255 0 0)
             (q/ellipse 0 0 (- (q/width) (:bleed state)) (- (q/height) (:bleed state)))
             (q/stroke 0 0 255)
-            (no-2))
+            ;; (no-2)
+            )
           (if (= (:method state) :lazer-cut) (q/exit))))
